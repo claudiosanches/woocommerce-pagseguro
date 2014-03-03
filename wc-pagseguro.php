058329008c217c94d8487b8a85d18e04af486f0c
claudiosmweb <contato@claudiosmweb.com>
2014-03-03 01:39:26 -0300

    improved the plugin main file

--- wc-pagseguro.php
+++ wc-pagseguro.php
@@ -5,72 +5,186 @@
  * Description: Gateway de pagamento PagSeguro para WooCommerce.
  * Author: claudiosanches, Gabriel Reguly
  * Author URI: http://claudiosmweb.com/
- * Version: 2.2.1
+ * Version: 2.3.0
  * License: GPLv2 or later
  * Text Domain: woocommerce-pagseguro
  * Domain Path: /languages/
  */
 
-/**
- * WooCommerce fallback notice.
- */
-function wcpagseguro_woocommerce_fallback_notice() {
-	echo '<div class="error"><p>' . sprintf( __( 'WooCommerce PagSeguro Gateway depends on the last version of %s to work!', 'woocommerce-pagseguro' ), '<a href="http://wordpress.org/extend/plugins/woocommerce/">WooCommerce</a>' ) . '</p></div>';
+if ( ! defined( 'ABSPATH' ) ) {
+	exit; // Exit if accessed directly.
 }
 
+if ( ! class_exists( 'WC_PagSeguro' ) ) :
+
 /**
- * Load functions.
+ * WooCommerce PagSeguro main class.
  */
-function wcpagseguro_gateway_load() {
+class WC_PagSeguro {
 
-	// Checks with WooCommerce is installed.
-	if ( ! class_exists( 'WC_Payment_Gateway' ) ) {
-		add_action( 'admin_notices', 'wcpagseguro_woocommerce_fallback_notice' );
+	/**
+	 * Plugin version.
+	 *
+	 * @since 2.3.0
+	 *
+	 * @var   string
+	 */
+	const VERSION = '2.3.0';
 
-		return;
+	/**
+	 * Integration id.
+	 *
+	 * @since 2.3.0
+	 *
+	 * @var   string
+	 */
+	protected static $gateway_id = 'pagseguro';
+
+	/**
+	 * Plugin slug.
+	 *
+	 * @since 2.3.0
+	 *
+	 * @var   string
+	 */
+	protected static $plugin_slug = 'woocommerce-pagseguro';
+
+	/**
+	 * Instance of this class.
+	 *
+	 * @since 2.3.0
+	 *
+	 * @var   object
+	 */
+	protected static $instance = null;
+
+	public function __construct() {
+		// Load plugin text domain
+		add_action( 'init', array( $this, 'load_plugin_textdomain' ) );
+
+		// Initialize the plugin actions.
+		$this->init();
 	}
 
 	/**
-	 * Load textdomain.
+	 * Return an instance of this class.
+	 *
+	 * @since  2.3.0
+	 *
+	 * @return object A single instance of this class.
 	 */
-	load_plugin_textdomain( 'woocommerce-pagseguro', false, dirname( plugin_basename( __FILE__ ) ) . '/languages/' );
+	public static function get_instance() {
+		// If the single instance hasn't been set, set it now.
+		if ( null == self::$instance ) {
+			self::$instance = new self;
+		}
+
+		return self::$instance;
+	}
+
+	/**
+	 * Return the plugin slug.
+	 *
+	 * @since  2.3.0
+	 *
+	 * @return string Plugin slug variable.
+	 */
+	public static function get_plugin_slug() {
+		return self::$plugin_slug;
+	}
+
+	/**
+	 * Return the gateway id/slug.
+	 *
+	 * @since  2.3.0
+	 *
+	 * @return string Gateway id/slug variable.
+	 */
+	public static function get_gateway_id() {
+		return self::$gateway_id;
+	}
+
+	/**
+	 * Load the plugin text domain for translation.
+	 *
+	 * @since  2.3.0
+	 *
+	 * @return void
+	 */
+	public function load_plugin_textdomain() {
+		$domain = self::$plugin_slug;
+		$locale = apply_filters( 'plugin_locale', get_locale(), $domain );
+
+		load_textdomain( $domain, trailingslashit( WP_LANG_DIR ) . $domain . '/' . $domain . '-' . $locale . '.mo' );
+		load_plugin_textdomain( $domain, false, dirname( plugin_basename( __FILE__ ) ) . '/languages/' );
+	}
+
+	/**
+	 * Initialize the plugin public actions.
+	 *
+	 * @since  2.3.0
+	 *
+	 * @return  void
+	 */
+	protected function init() {
+		// Checks with WooCommerce is installed.
+		if ( class_exists( 'WC_Payment_Gateway' ) ) {
+			// Include the WC_PagSeguro_Gateway class.
+			include_once 'includes/class-wc-pagseguro-gateway.php';
+
+			add_filter( 'woocommerce_payment_gateways', array( $this, 'add_gateway' ) );
+			add_filter( 'woocommerce_available_payment_gateways', array( $this, 'hides_when_is_outside_brazil' ) );
+		} else {
+			add_action( 'admin_notices', array( $this, 'woocommerce_missing_notice' ) );
+		}
+	}
 
 	/**
 	 * Add the gateway to WooCommerce.
 	 *
-	 * @param  array $methods WooCommerce payment methods.
+	 * @version 2.3.0
 	 *
-	 * @return array          Payment methods with PagSeguro.
+	 * @param   array $methods WooCommerce payment methods.
+	 *
+	 * @return  array          Payment methods with PagSeguro.
 	 */
-	function wcpagseguro_add_gateway( $methods ) {
+	public function add_gateway( $methods ) {
 		$methods[] = 'WC_PagSeguro_Gateway';
 
 		return $methods;
 	}
 
-	add_filter( 'woocommerce_payment_gateways', 'wcpagseguro_add_gateway' );
-
-	// Include the WC_PagSeguro_Gateway class.
-	require_once plugin_dir_path( __FILE__ ) . 'includes/class-wc-pagseguro-gateway.php';
-}
-
-add_action( 'plugins_loaded', 'wcpagseguro_gateway_load', 0 );
-
-/**
- * Hides the PagSeguro with payment method with the customer lives outside Brazil
- *
- * @param  array $available_gateways Default Available Gateways.
- *
- * @return array                    New Available Gateways.
- */
-function wcpagseguro_hides_when_is_outside_brazil( $available_gateways ) {
-
-	// Remove standard shipping option.
-	if ( isset( $_REQUEST['country'] ) && 'BR' != $_REQUEST['country'] ) {
-		unset( $available_gateways['pagseguro'] );
+	/**
+	 * WooCommerce fallback notice.
+	 *
+	 * @version 2.3.0
+	 *
+	 * @return  string
+	 */
+	public function woocommerce_missing_notice() {
+		echo '<div class="error"><p>' . sprintf( __( 'WooCommerce PagSeguro Gateway depends on the last version of %s to work!', self::$plugin_slug ), '<a href="http://wordpress.org/extend/plugins/woocommerce/">WooCommerce</a>' ) . '</p></div>';
 	}
 
-	return $available_gateways;
+	/**
+	 * Hides the PagSeguro with payment method with the customer lives outside Brazil.
+	 *
+	 * @version 2.3.0
+	 *
+	 * @param   array $available_gateways Default Available Gateways.
+	 *
+	 * @return  array                     New Available Gateways.
+	 */
+	public function hides_when_is_outside_brazil( $available_gateways ) {
+
+		// Remove PagSeguro gateway.
+		if ( isset( $_REQUEST['country'] ) && 'BR' != $_REQUEST['country'] ) {
+			unset( $available_gateways['pagseguro'] );
+		}
+
+		return $available_gateways;
+	}
 }
 
-add_filter( 'woocommerce_available_payment_gateways', 'wcpagseguro_hides_when_is_outside_brazil' );
+add_action( 'plugins_loaded', array( 'WC_PagSeguro', 'get_instance' ), 0 );
+
+endif;
