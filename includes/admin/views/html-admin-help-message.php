d8712b47c1ae39ddf24bcc6db26ae07d8a5a9dcc
Claudio Sanches <contato@claudiosmweb.com>
2016-03-03 20:57:35 -0300

    Fixed inline message

--- includes/admin/views/html-admin-help-message.php
+++ includes/admin/views/html-admin-help-message.php
@@ -10,7 +10,7 @@ if ( ! defined( 'ABSPATH' ) ) {
 }
 
 if ( apply_filters( 'woocommerce_pagseguro_help_message', true ) ) : ?>
-	<div class="updated woocommerce-message">
+	<div class="updated inline woocommerce-message">
 		<p><?php echo esc_html( sprintf( __( 'Help us keep the %s plugin free making a donation or rate %s on WordPress.org. Thank you in advance!', 'woocommerce-pagseguro' ), __( 'WooCommerce PagSeguro', 'woocommerce-pagseguro' ), '&#9733;&#9733;&#9733;&#9733;&#9733;' ) ); ?></p>
 		<p><a href="http://claudiosmweb.com/doacoes/" target="_blank" class="button button-primary"><?php esc_html_e( 'Make a donation', 'woocommerce-pagseguro' ); ?></a> <a href="https://wordpress.org/support/view/plugin-reviews/woocommerce-pagseguro?filter=5#postform" target="_blank" class="button button-secondary"><?php esc_html_e( 'Make a review', 'woocommerce-pagseguro' ); ?></a></p>
 	</div>
