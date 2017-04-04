a3e96f2635c4f1901ca360e9373615af906c9eec
Claudio Sanches <contato@claudiosanches.com>
2017-04-04 00:05:28 -0300

    Fixed coding standards in transparent-checkout.js

--- assets/js/transparent-checkout.js
+++ assets/js/transparent-checkout.js
@@ -92,16 +92,16 @@
 			$( '#pagseguro-card-holder-birth-date' ).mask( '00/00/0000' );
 
 			// Phone.
-			var MaskBehavior = function(val) {
-						return val.replace(/\D/g, '').length === 11 ? '(00) 00000-0000' : '(00) 0000-00009';
-					},
-					maskOptions = {
-						onKeyPress: function(val, e, field, options) {
-							field.mask(MaskBehavior.apply({}, arguments), options);
-						}
-					};
+			var MaskBehavior = function( val ) {
+					return val.replace( /\D/g, '' ).length === 11 ? '(00) 00000-0000' : '(00) 0000-00009';
+				},
+				maskOptions = {
+					onKeyPress: function( val, e, field, options ) {
+						field.mask( MaskBehavior.apply( {}, arguments ), options );
+					}
+				};
 
-			$( '#pagseguro-card-holder-phone' ).mask(MaskBehavior, maskOptions);
+			$( '#pagseguro-card-holder-phone' ).mask( MaskBehavior, maskOptions );
 
 			$( '#pagseguro-bank-transfer-form input[type=radio]:checked' ).parent( 'label' ).parent( 'li' ).addClass( 'active' );
 		}
